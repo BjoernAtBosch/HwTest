c25f6ccb74331ba50d9a05cb132b09481fe791d4
BjoernAtBosch <Bjoern.Hornburg@de.bosch.com>
2023-12-01 15:18:00 +0100

    Add app

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Robert Bosch GmbH and Microsoft Corporation
+# Copyright (c) 2023 Robert Bosch GmbH
 #
 # This program and the accompanying materials are made available under the
 # terms of the Apache License, Version 2.0 which is available at
@@ -12,12 +12,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""A sample skeleton vehicle app."""
 
+# pylint: disable=C0103, C0413, E1101
 import asyncio
-import json
 import logging
 import signal
+from enum import Enum
 
 from vehicle import Vehicle, vehicle  # type: ignore
 from velocitas_sdk.util.log import (  # type: ignore
@@ -25,7 +25,7 @@ from velocitas_sdk.util.log import (  # type: ignore
     get_opentelemetry_log_format,
 )
 from velocitas_sdk.vdb.reply import DataPointReply
-from velocitas_sdk.vehicle_app import VehicleApp, subscribe_topic
+from velocitas_sdk.vehicle_app import VehicleApp
 
 # Configure the VehicleApp logger with the necessary log config and level.
 logging.setLogRecordFactory(get_opentelemetry_log_factory())
@@ -33,91 +33,60 @@ logging.basicConfig(format=get_opentelemetry_log_format())
 logging.getLogger().setLevel("DEBUG")
 logger = logging.getLogger(__name__)
 
-GET_SPEED_REQUEST_TOPIC = "sampleapp/getSpeed"
-GET_SPEED_RESPONSE_TOPIC = "sampleapp/getSpeed/response"
-DATABROKER_SUBSCRIPTION_TOPIC = "sampleapp/currentSpeed"
+
+class Mode(Enum):
+    OFF = 0
+    NIGHT_RIDER = 1
+    RAINBOW = 2
+    FADE_IN = 3
 
 
-class SampleApp(VehicleApp):
-    """
-    Sample skeleton vehicle app.
-
-    The skeleton subscribes to a getSpeed MQTT topic
-    to listen for incoming requests to get
-    the current vehicle speed and publishes it to
-    a response topic.
-
-    It also subcribes to the VehicleDataBroker
-    directly for updates of the
-    Vehicle.Speed signal and publishes this
-    information via another specific MQTT topic
-    """
-
+class PassengerWelcomeApp(VehicleApp):
     def __init__(self, vehicle_client: Vehicle):
-        # SampleApp inherits from VehicleApp.
         super().__init__()
         self.Vehicle = vehicle_client
+        self.driver_door = self.Vehicle.Cabin.Door.Row1.Left
+        self.driver_seat = self.Vehicle.Cabin.Seat.Row1.Pos1
 
     async def on_start(self):
-        """Run when the vehicle app starts"""
-        # This method will be called by the SDK when the connection to the
-        # Vehicle DataBroker is ready.
-        # Here you can subscribe for the Vehicle Signals update (e.g. Vehicle Speed).
-        await self.Vehicle.Speed.subscribe(self.on_speed_change)
+        await self.driver_seat.Position.set(0)
 
-    async def on_speed_change(self, data: DataPointReply):
-        """The on_speed_change callback, this will be executed when receiving a new
-        vehicle signal updates."""
-        # Get the current vehicle speed value from the received DatapointReply.
-        # The DatapointReply containes the values of all subscribed DataPoints of
-        # the same callback.
-        vehicle_speed = data.get(self.Vehicle.Speed).value
+        # Callback
+        await self.driver_door.IsOpen.subscribe(self.on_door_status_change)
 
-        # Do anything with the received value.
-        # Example:
-        # - Publishes current speed to MQTT Topic (i.e. DATABROKER_SUBSCRIPTION_TOPIC).
-        await self.publish_event(
-            DATABROKER_SUBSCRIPTION_TOPIC,
-            json.dumps({"speed": vehicle_speed}),
-        )
+    async def on_door_status_change(self, data: DataPointReply):
+        """This will be executed when receiving a new door status update."""
+        door_status = data.get(self.driver_door.IsOpen).value
 
-    @subscribe_topic(GET_SPEED_REQUEST_TOPIC)
-    async def on_get_speed_request_received(self, data: str) -> None:
-        """The subscribe_topic annotation is used to subscribe for incoming
-        PubSub events, e.g. MQTT event for GET_SPEED_REQUEST_TOPIC.
-        """
-
-        # Use the logger with the preferred log level (e.g. debug, info, error, etc)
-        logger.debug(
-            "PubSub event for the Topic: %s -> is received with the data: %s",
-            GET_SPEED_REQUEST_TOPIC,
-            data,
-        )
-
-        # Getting current speed from VehicleDataBroker using the DataPoint getter.
-        vehicle_speed = (await self.Vehicle.Speed.get()).value
-
-        # Do anything with the speed value.
-        # Example:
-        # - Publishes the vehicle speed to MQTT topic (i.e. GET_SPEED_RESPONSE_TOPIC).
-        await self.publish_event(
-            GET_SPEED_RESPONSE_TOPIC,
-            json.dumps(
-                {
-                    "result": {
-                        "status": 0,
-                        "message": f"""Current Speed = {vehicle_speed}""",
-                    },
-                }
-            ),
-        )
+        if door_status:
+            await self.Vehicle.set_many().add(self.driver_seat.Position, 1000).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Mode, str(Mode.NIGHT_RIDER.value)
+            ).add(self.Vehicle.Cabin.Lights.InteriorLight.Red, 0).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Green, 255
+            ).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Blue, 0
+            ).add(
+                self.Vehicle.Body.Lights.Beam.Low.IsOn, True
+            ).apply()
+        else:
+            await self.Vehicle.set_many().add(
+                self.driver_seat.Position, 500
+            ).add(self.Vehicle.Cabin.Lights.InteriorLight.Mode, str(Mode.RAINBOW.value)).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Red, 255
+            ).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Green, 0
+            ).add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Blue, 0
+            ).apply()
+            await asyncio.sleep(2)
+            await self.Vehicle.set_many().add(
+                self.Vehicle.Cabin.Lights.InteriorLight.Mode, str(Mode.OFF.value)
+            ).add(self.Vehicle.Body.Lights.Beam.Low.IsOn, False).apply()
 
 
 async def main():
-    """Main function"""
-    logger.info("Starting SampleApp...")
-    # Constructing SampleApp and running it.
-    vehicle_app = SampleApp(vehicle)
+    logger.info("Starting PassengerWelcomeApp...")
+    vehicle_app = PassengerWelcomeApp(vehicle)
     await vehicle_app.run()
 
 
